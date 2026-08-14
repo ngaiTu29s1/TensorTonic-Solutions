a7a1848afec054632fb1589d8ac210248fbd424a
Tran Tuan Tu <trantuantu2004@gmail.com>
2026-08-14 17:04:33 +0700

    Solve logistic-regression-training

---
+++ logistic-regression-training/logistic-regression-training.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+def _sigmoid(z):
+    """Numerically stable sigmoid implementation."""
+    return np.where(z >= 0, 1/(1+np.exp(-z)), np.exp(z)/(1+np.exp(z)))
+
+def train_logistic_regression(X, y, lr=0.1, steps=1000):
+    """
+    Train logistic regression via gradient descent.
+    Return (w, b).
+    """
+    # Write code here
+    n_samples, n_features = X.shape
+
+    w = np.zeros(n_features)
+    b = 0.0
+
+    for _ in range(steps):
+        z = X @ w + b 
+
+        y_pred = _sigmoid(z)
+
+        error = y_pred - y
+
+        dw = (X.T @ error) / n_samples
+        db = np.mean(error)
+
+        w -= lr*dw
+        b -= lr*db
+
+    return w, b
